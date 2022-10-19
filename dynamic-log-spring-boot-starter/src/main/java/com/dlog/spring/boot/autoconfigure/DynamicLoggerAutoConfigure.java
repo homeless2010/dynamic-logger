ce35a7ebd390d8ad0eca369d9d95755c9af868e3
homeless2010 <paulvsjames@gmail.com>
2022-10-19 09:54:18 +0800

    fix: DynamicLoggerAutoConfigure.java

--- dynamic-log-spring-boot-starter/src/main/java/com/dlog/spring/boot/autoconfigure/DynamicLoggerAutoConfigure.java
+++ dynamic-log-spring-boot-starter/src/main/java/com/dlog/spring/boot/autoconfigure/DynamicLoggerAutoConfigure.java
@@ -1,8 +1,8 @@
 package com.dlog.spring.boot.autoconfigure;
 
-import com.step.dlog.controller.LoggerLevelController;
-import com.step.dlog.spring.boot.autoconfigure.properties.DynamicLoggerProperties;
-import com.step.dlog.spring.boot.autoconfigure.web.DynamicLoggerAuthFilterConfiguration;
+import com.dlog.controller.LoggerLevelController;
+import com.dlog.spring.boot.autoconfigure.properties.DynamicLoggerProperties;
+import com.dlog.spring.boot.autoconfigure.web.DynamicLoggerAuthFilterConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
